d2e71e7de761f0bd0ae08d429d8b973fc41b6d6c
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-05 22:50:06 +0530

    Final Card. java file

--- Card.java
+++ Card.java
@@ -1,1 +1,36 @@
+// Card.java
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+    
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void printCard() {
+        System.out.println(rank + " of " + suit);
+    }
+
+    public boolean sameSuit(Card other) {
+        return this.suit.equals(other.suit);
+    }
+
+
+    public boolean sameRank(Card other) {
+        return this.rank.equals(other.rank);
+    }
+
+    public boolean equals(Card other) {
+        return this.suit.equals(other.suit) && this.rank.equals(other.rank);
+    }
+}
 
