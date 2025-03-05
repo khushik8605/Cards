a5f80b66715e7aa7265fd08eb93b5ad614b63d92
khushik8605 <142583464+khushik8605@users.noreply.github.com>
2025-03-05 22:46:34 +0530

    creating base structure

--- Deck.java
+++ Deck.java
@@ -1,1 +1,22 @@
+import java.util.*;
+
+public class Deck {
+    private ArrayList<Card> deck;
+    private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+    }
+
+    public void createDeck() {
+        deck.clear();
+        for (String suit : SUITS) {
+            for (String rank : RANKS) {
+                deck.add(new Card(suit, rank));
+            }
+        }
+    }
+}
 
